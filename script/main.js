0374464d2110bf4fb3fcccc57682f9cda808338b
Ege Durukan <egedurukan@gmail.com>
2020-03-26 14:43:24 +0100

    express had been setup on main.js, now index.html will be sent to the main root

--- script/main.js
+++ script/main.js
@@ -1,1 +1,17 @@
 'use strict'
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 8080;
+
+app.listen(PORT, (req, res) => {
+  console.log(`App is running on: ${PORT}`);
+});
+
+app.use(express.static('assests'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
